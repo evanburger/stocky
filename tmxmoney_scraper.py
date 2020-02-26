ecceea098e4ebfa6aacb6e3673526933c88c5dc8
Evan Mildenberger <emildenberger>
2020-02-26 13:19:07 +0300

    Update directory to match old project directory.
    This repo doesn't have personal information hard-coded into it, but works like the previous one.

---
+++ tmxmoney_scraper.py
@@ -0,0 +1,20 @@
+import csv
+import io
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_tmxmoney(symbol):
+    url = "https://web.tmxmoney.com/quote.php?qm_symbol={symbol}".format(symbol=symbol)
+    response = requests.get(url).text
+    soup = BeautifulSoup(response, "html.parser")
+
+    outer_span_tag = soup.find("span", class_="price")
+    span_tag_text = outer_span_tag.find("span").text
+    
+    try:
+        data_float = float(span_tag_text)
+        return data_float
+    except ValueError as e:
+        raise e

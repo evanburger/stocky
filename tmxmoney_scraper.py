69cc887053b3951b1764c264bd8eff2663080dca
Evan Mildenberger <emildenberger>
2020-09-04 14:23:26 +0300

    Modify tmxmoney_scraper.py, stocky.
    The TMX site has been updated and now the HTML can only be rendered in a browser. So beautifulsoup has now been replaced with Selenium. The functions in tmxmoney_scraper were also refactored and the names updated in stocky.

--- tmxmoney_scraper.py
+++ tmxmoney_scraper.py
@@ -1,20 +1,37 @@
-import csv
-import io
+import os
+import time
 
-import requests
-from bs4 import BeautifulSoup
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-def scrape_tmxmoney(symbol):
-    url = "https://web.tmxmoney.com/quote.php?qm_symbol={symbol}".format(symbol=symbol)
-    response = requests.get(url).text
-    soup = BeautifulSoup(response, "html.parser")
+DRIVER_PATH = os.environ["CHROMEDRIVER_LOCATION"]
 
-    outer_span_tag = soup.find("span", class_="price")
-    span_tag_text = outer_span_tag.find("span").text
-    
+
+def access_site(symbol):
+    """Return the Chrome driver open to the TMX quote for the given symbol."""
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(DRIVER_PATH, chrome_options=chrome_options)
+    url = "https://money.tmx.com/en/quote/{symbol}".format(symbol=symbol)
+    driver.get(url)
+    return driver
+
+def refresh_site(driver):
+    """Return the Chrome driver after refreshing the page with the TMX quote for the given symbol."""
+    driver.refresh()
+    return driver
+
+def parse_data(driver):
+    """Return a float of the current price given the driver open to the TMX page of the specific symbol."""
+    # The driver needs time to load the page before it can be parsed.
+    time.sleep(5)
+    content_obj = driver.find_element(by="id", value="root")
+    content_text = content_obj.text
+    price_string = content_text.split("PRICE")[1].split("CHANGE")[0].strip()
     try:
-        data_float = float(span_tag_text.replace(",", "_"))
-        return data_float
+        price_float = float(price_string.replace("$", "").replace(",", ""))
+        return price_float
     except ValueError as e:
         raise e
